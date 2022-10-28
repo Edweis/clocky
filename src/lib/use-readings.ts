5c0ae77379203140eacc6d0319d869e716eb98e3
François Rullière <kapochamo@gmail.com>
2022-10-28 13:50:13 +0800

    10sec

--- src/lib/use-readings.ts
+++ src/lib/use-readings.ts
@@ -1,4 +1,4 @@
 import { Reading } from '../types';
 import { SangriaReact } from './sangria/react';
 
-export const SangriaReadings = new SangriaReact<Reading>('readings', 5000);
+export const SangriaReadings = new SangriaReact<Reading>('readings', 10000);
